76617ac68891a4c9942bb9ffddd63e95ac223ab7
pmuwanguzi <p.muwanguzi@alustudent.com>
2025-05-28 11:34:10 +0200

    updated string

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,14 @@ class BaseModel:
     """ 
     Intialising the object
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """
+        Initialization method for BaseModel.
+        Paublic Instance attributes:
+            id: a unique identifier 
+            created at
+            updated up
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
