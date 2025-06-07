96008251d7ac144a701a26810189e2da5dc63e80
pmuwanguzi <p.muwanguzi@alustudent.com>
2025-06-07 23:35:59 +0200

    added comments

--- models/engine/flie_storage.py
+++ models/engine/flie_storage.py
@@ -17,6 +17,16 @@ class FileStorage:
         """
         return self.__objects
     def new(self,new_obj):
+        """
+        Adds a new object to the FileStorage's dictionary of objects.
+        The new object is stored in the dictionary with a key in the format.
+        <class name>.<object id>. The object is then saved to disk
+
+        Parameters
+        -----------
+        new_obj : BaseModel
+            The new object to be added to the dictionary of objects.
+        """
         key = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
         self.__objects[key] = new_obj
 
